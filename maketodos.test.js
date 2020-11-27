e31e380953c4e3bf6cff00e60078175f18723780
CodingBee <2395250395@qq.com>
2020-11-27 21:43:53 +0800

    更新了 test 文件

--- maketodos.test.js
+++ maketodos.test.js
@@ -1,4 +1,14 @@
-const formsubmit = require('./maketodos')
-const {formSubmit} = formsubmit
+import '../maketodos';
 
-test
+const formsubmit = require('./maketodos');
+const {formSubmit} = formsubmit;
+const page = global.wxPageInstance;
+// mock 网络请求
+jest.mock('./maketodos.js');
+
+describe('新增事项表单提交', () => {
+  beforeAll( () => {
+    jest.spyOn(page, 'formSubmit');
+    page.onload();
+  });
+});
